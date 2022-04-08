97be059e39ef97ea11cf8154a15fb42ee4f47b17
Rasmus Hoier <rasmasmus@gmail.com>
2022-04-08 15:39:23 +0200

    renamed functions and edited some docstrings

--- src/RogueLearning.jl
+++ src/RogueLearning.jl
@@ -6,8 +6,7 @@ using Zygote: pullback
 import NNlib: conv
 # import Base: *
 
-export GenDense, GenConv, dfa_loss, radialSim, matmul,
-    conv
+export GenDense, GenConv, direct_feedback_loss, radialSim, radialSim_asym, matmul, matmul_asym_∂x, matmul_blocked_∂x, conv_asym_∂x
 
 include("layers.jl")
 include("functions.jl")
--- src/functions.jl
+++ src/functions.jl
@@ -16,7 +16,7 @@ end
 """
 In the forward pass this function behaves just like radialSim, but in the backwards pass weight symmetry is broken by using matrix B rather than Wᵀ. See docstring for radialSim for more details.
 """
-function radialSim(W, X, B)
+function radialSim_asym(W, X, B)
     x2 = sum(abs2, X, dims=1) # sum the elements of each column
     W2 = sum(abs2, W, dims=2) # sum the elements of each row
     D = 2*matmul(W,X,B) .- W2 .- x2
@@ -37,14 +37,14 @@ function matmul(W, X)
 Compute matrix multiplication, but takes an additional matrix B as input. 
 B has same dims as Wᵀ, and is used in the backwards pass.
 """
-function matmul(W, X, B) 
+function matmul_asym_∂x(W, X, B) 
     return W * X
  end
 
  """
  rrule which uses feedback weights B instead of Wᵀ, which regular backpropagation would use.
  """
- function ChainRulesCore.rrule(::typeof(matmul), W::AbstractMatrix, X::AbstractMatrix, B::AbstractMatrix)
+ function ChainRulesCore.rrule(::typeof(matmul_asym_∂x), W::AbstractMatrix, X::AbstractMatrix, B::AbstractMatrix)
     y = matmul(W, X)
     function times_pullback(ΔΩ)
        ∂W = @thunk(ΔΩ * X')
@@ -55,20 +55,21 @@ function matmul(W, X, B)
  end
 
  """
- conv called with an additional feedback weight matrix. The forward pass gives the same results, but in the backwards pass the gradient will be different thanks to multiple dispatch.
+computes the convolution of image x with kernel `w` when called, but uses a different set of weights `w_asym`
+to compute the pullback wrt x. This is typically uses in feedback alignment experiments.
  """
- function conv(x, w::AbstractArray{T, N}, wFB::AbstractArray{T, N}, cdims) where {T, N}
+ function conv_asym_∂x(x, w::AbstractArray{T, N}, w_asym::AbstractArray{T, N}, cdims) where {T, N}
     return conv(x, w, cdims)
 end
 
  """
- When conv is called with an additional weight matrix wFB, then we will use this custom rrule, which transports error backwards using wFB instead of W.
+rrule for conv_asym.
  """
- function ChainRulesCore.rrule(::typeof(conv), x, w, wFB, cdims)
+ function ChainRulesCore.rrule(::typeof(conv_asym_∂x), x, w, w_asym, cdims)
     z = conv(x, w, cdims)
     function pullback(Δy)
-        # When computing ∇conv_data the the feedback weights wFB are used instead of W. This is the main idea of feedback alignment.
-        return NoTangent(), ∇conv_data(Δy, wFB, cdims), ∇conv_filter(x, Δy, cdims), NoTangent(), NoTangent()
+        # When computing ∇conv_data the the feedback weights w_asym are used instead of W. This is the main idea of feedback alignment.
+        return NoTangent(), ∇conv_data(Δy, w_asym, cdims), ∇conv_filter(x, Δy, cdims), NoTangent(), NoTangent()
     end
     return z, pullback
 end
@@ -76,17 +77,17 @@ end
 """
 Matrix multiplication with custom rrule
 """
-matmul_dfa(W, X) = return W * X
+matmul_blocked_∂x(W, X) = return W * X
 
 """
 This rrule blocks the regular backprop pathway by returning NoTangent() instead of ∂X
 """
-function ChainRulesCore.rrule(::typeof(matmul_dfa), W::AbstractMatrix, X::AbstractMatrix)
+function ChainRulesCore.rrule(::typeof(matmul_blocked_∂x), W::AbstractMatrix, X::AbstractMatrix)
     y = W*X
-    function matmul_dfa_pullback(ΔΩ::AbstractMatrix{Float32})
+    function matmul_blocked_∂x_pullback(ΔΩ::AbstractMatrix{Float32})
         ΔΩ = unthunk(ΔΩ)
         ∂W = @thunk(ΔΩ * X')
         return (NoTangent(), ∂W, NoTangent())
     end
-    return y, matmul_dfa_pullback
+    return y, matmul_blocked_∂x_pullback
 end
