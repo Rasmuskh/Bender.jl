4349ce736c27d32d97c4f94cd7df82c3ec575298
Rasmus <rasmasmus@gmail.com>
2022-04-14 14:21:09 +0200

    updating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,8 @@ using Test
     x = rand(Float32, 3, 8)
     y = rand(Float32, 2, 8)
 
-    my_radial(a, x) = a.σ.(radialSim(a.weight, x) .+ a.bias)
-
-    m1 = Chain( GenDense(3=>4, σ; forward=my_radial), 
-                GenDense(4=>2, σ; forward=my_radial))
-    # (a::GenDense)(x::AbstractVecOrMat) = a.σ.(a.ψ(a.ω.(a.weight), x) .+ a.bias)
+    m1 = Chain( GenDense(3=>4, σ; forward=radial), 
+                GenDense(4=>2, σ; forward=radial))
 
     gs1 = gradient(Flux.params(m1)) do
         Flux.Losses.mse(m1(x), y)
@@ -19,11 +16,8 @@ using Test
     @test size(gs1[m1[1].weight]) == size(m1[1].weight)
     @test size(gs1[m1[2].weight]) == size(m1[2].weight)
 
-    my_radial_asym(a, x) = a.σ.(radialSim_asym(a.weight, x, a.weight_asym) .+ a.bias)
-
-    m2 = Chain( GenDense(3=>4, 4=>3, σ; forward=my_radial_asym), 
-                GenDense(4=>2, 2=>4, σ; forward=my_radial_asym))
-    # (a::GenDense)(x::AbstractVecOrMat) = a.σ.(a.ψ(a.ω.(a.weight), x, a.weight_asym) .+ a.bias)
+    m2 = Chain( GenDense(3=>4, 4=>3, σ; forward=radial_asym_∂x), 
+                GenDense(4=>2, 2=>4, σ; forward=radial_asym_∂x))
 
     gs2 = gradient(Flux.params(m2)) do
         sum(abs2, m2(x))
