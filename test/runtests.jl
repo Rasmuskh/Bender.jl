ed80d235883bc537203042296d7aafefdd4afafd
Rasmus <rasmasmus@gmail.com>
2022-04-14 13:54:09 +0200

    updating tests following GenDense API change

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,12 @@ using Test
 @testset "Test RBF chain" begin
     x = rand(Float32, 3, 8)
     y = rand(Float32, 2, 8)
-    
-    m1 = Chain(GenDense(3=>4, σ; ω=tanh, ψ=radialSim), GenDense(4=>2, σ; ω=tanh, ψ=radialSim))
-    (a::GenDense)(x::AbstractVecOrMat) = a.σ.(a.ψ(a.ω.(a.weight), x) .+ a.bias)
+
+    my_radial(a, x) = a.σ.(radialSim(a.weight, x) .+ a.bias)
+
+    m1 = Chain( GenDense(3=>4, σ; forward=my_radial), 
+                GenDense(4=>2, σ; forward=my_radial))
+    # (a::GenDense)(x::AbstractVecOrMat) = a.σ.(a.ψ(a.ω.(a.weight), x) .+ a.bias)
 
     gs1 = gradient(Flux.params(m1)) do
         Flux.Losses.mse(m1(x), y)
@@ -16,9 +19,11 @@ using Test
     @test size(gs1[m1[1].weight]) == size(m1[1].weight)
     @test size(gs1[m1[2].weight]) == size(m1[2].weight)
 
+    my_radial_asym(a, x) = a.σ.(radialSim_asym(a.weight, x, a.weight_asym) .+ a.bias)
 
-    m2 = Chain(GenDense(3=>4, 4=>3, σ; ω=tanh, ψ=radialSim_asym), GenDense(4=>2, 2=>4, σ; ω=tanh, ψ=radialSim_asym))
-    (a::GenDense)(x::AbstractVecOrMat) = a.σ.(a.ψ(a.ω.(a.weight), x, a.weight_asym) .+ a.bias)
+    m2 = Chain( GenDense(3=>4, 4=>3, σ; forward=my_radial_asym), 
+                GenDense(4=>2, 2=>4, σ; forward=my_radial_asym))
+    # (a::GenDense)(x::AbstractVecOrMat) = a.σ.(a.ψ(a.ω.(a.weight), x, a.weight_asym) .+ a.bias)
 
     gs2 = gradient(Flux.params(m2)) do
         sum(abs2, m2(x))
